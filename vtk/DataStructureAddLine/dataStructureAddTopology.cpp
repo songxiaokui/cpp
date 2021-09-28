67b686d68e66585306f164e3a8267626c979a76b
Austin <austin.song@yuansuan.cn>
2021-09-28 10:36:46 +0800

    #给线添加矢量数据

--- vtk/DataStructureAddLine/dataStructureAddTopology.cpp
+++ vtk/DataStructureAddLine/dataStructureAddTopology.cpp
@@ -7,17 +7,21 @@
 #include <vtkCellArray.h>
 #include <vtkPolyDataWriter.h>
 #include <vtkLine.h>
+#include <vtkFloatArray.h>
+#include <vtkIntArray.h>
+#include <vtkCellData.h>
+#include <vtkPointData.h>
 using namespace std;
 // 将零维拓扑图改为一维拓扑图
 int main() {
     // 创建三个坐标点
     vtkNew<vtkPoints> points;
     points->InsertNextPoint(1, 0, 0);
-    points->InsertNextPoint(0, 1, 0);
-    points->InsertNextPoint(0, 0, 0);
+    points->InsertNextPoint(2, 0, 0);
+    points->InsertNextPoint(3, 0, 0);
 
     // 创建一个多边形数据集
-    vtkNew<vtkPolyData> polyData;
+    vtkSmartPointer<vtkPolyData> polyData = vtkSmartPointer<vtkPolyData>::New();
     polyData->SetPoints(points);
 
     // 创建3条线,每两个点连一条线 SetId第一个参数是线段的端点id 第二个参数为点索引编号
@@ -40,6 +44,32 @@ int main() {
     lineArray->InsertNextCell(line2);
 
     polyData->SetLines(lineArray);
+
+    // 给线条上点添加标量数据
+    //创建一个可以存储浮点型数据的标量属性数据数组（组元为1，及一维数据）
+    vtkNew<vtkFloatArray> floatArrScalar;
+    //向数据尾部插入浮点型数据
+    floatArrScalar->InsertTuple1(0, 1.0);
+    floatArrScalar->InsertTuple1(1, 2.0);
+    floatArrScalar->InsertTuple1(2, 3.0);
+    floatArrScalar->SetName("B");
+
+    //创建一个可以存储浮点型数据的矢量属性数据数组（组元为3，及三维数据）
+    vtkNew<vtkFloatArray> floatArrVector;
+    //设置组元大小为3
+    floatArrVector->SetNumberOfComponents(3);
+    //向数据尾部插入浮点型向量
+    floatArrVector->InsertTuple3(0, 1.0, 1.0, 1.0);
+    floatArrVector->InsertTuple3(1, 2.0, 2.0, 2.0);
+    floatArrVector->InsertTuple3(2, 3.0, 3.0, 3.0);
+    floatArrVector->SetName("A");
+
+    //将标量属性数据与数据集中的点相关联
+    //创建多边形数据集对象
+    //将标量属性数据设置到数据点集中
+    polyData->GetPointData()->SetScalars(floatArrScalar);
+    //将矢量属性数据设置到单元数据集中
+    polyData->GetCellData()->SetVectors(floatArrVector);
 
     vtkNew<vtkPolyDataWriter> writer;
     writer->SetFileName("pointsAndLines.vtk");
---
+++ vtk/line/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by austsxk on 2021/8/9.
+//
+
+// 创建一个线 将其转化为三角形
+
+int main() {
+
+}
